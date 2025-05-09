c20f1280f8e88ff4daeda21d817872af778e47ae
Keegan Hinton <khinton386@gmail.com>
2025-05-09 16:23:34 -0400

    added in a categories section with a dropdown menu for readers to choose what categories they want to read

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,47 @@
+import React, { useState } from 'react';
+import '../assets/Header.css'; // We'll create this CSS file next
+
 export default function Header() {
-    return (
-      <header className="header">
-        <div className="logo">BookCraft</div>
-        <nav>
-          <a href="#features">Features</a>
-          <a href="#publish">Publish</a>
-          <button className="login-btn">Author Login</button>
-        </nav>
-      </header>
-    );
-  }
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const categories = [
+    { name: 'Books', link: '/books' },
+    { name: 'Short Stories', link: '/short-stories' },
+    { name: 'Comics', link: '/comics' },
+    { name: 'Mangas', link: '/mangas' },
+    { name: 'Manhwas', link: '/manhwas' }
+  ];
+
+  return (
+    <header className="header">
+      <div className="logo">BookCraft</div>
+      <nav>
+        <div 
+          className="categories-dropdown"
+          onMouseEnter={() => setIsDropdownOpen(true)}
+          onMouseLeave={() => setIsDropdownOpen(false)}
+        >
+          <button className="categories-button">
+            Categories
+          </button>
+          {isDropdownOpen && (
+            <div className="dropdown-menu">
+              {categories.map((category) => (
+                <a 
+                  key={category.name} 
+                  href={category.link}
+                  className="dropdown-item"
+                >
+                  {category.name}
+                </a>
+              ))}
+            </div>
+          )}
+        </div>
+        <a href="#features">Features</a>
+        <a href="#publish">Publish</a>
+        <button className="login-btn">Author Login</button>
+      </nav>
+    </header>
+  );
+}
